79b02cefc22ae43f3e9fb3083d01b8742e15468c
Oliver Andreas Thunæs <oliverat@ostfoldfk.no>
2016-08-26 23:54:39 +0200

    added test 'about' page

--- testapp/views.py
+++ testapp/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there world!")
+    return HttpResponse("<b>Rango</b> says hey there world! <br> <a href='/testapp/about'>About!</a>")
+	
+def about(request):
+	return HttpResponse("Rango says that this is the about page! <br> <a href='/testapp'>Return!</a>")
