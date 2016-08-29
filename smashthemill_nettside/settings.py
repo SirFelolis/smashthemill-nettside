b280c542a8566930412c6a1ab5149666d0b8d28e
Oliver Thunæs <Felolis30@gmail.com>
2016-08-29 21:14:08 +0200

    Added templates

--- smashthemill_nettside/settings.py
+++ smashthemill_nettside/settings.py
@@ -15,6 +15,9 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+print(BASE_DIR)
+# Holds the path to the templates folder
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
@@ -55,7 +58,7 @@ ROOT_URLCONF = 'smashthemill_nettside.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR, ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
