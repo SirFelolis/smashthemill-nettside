bcd19e1d4af15fcfc79b5db032b1cf16f19b2451
Oliver Thunæs <Felolis30@gmail.com>
2016-08-29 20:32:01 +0200

    Actually removed testapp

--- testapp/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def index(request):
-    return HttpResponse("<b>Rango</b> says hey there world! <br> <a href='/testapp/about'>About!</a>")
-	
-def about(request):
-	return HttpResponse("Rango says that this is the about page! <br> <a href='/testapp'>Return!</a>")
